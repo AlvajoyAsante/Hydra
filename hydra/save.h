0763436584b7e4de0f5c85650fa380b668deface
Alvajoy Asante <33734347+Overload02@users.noreply.github.com>
2023-04-12 15:53:23 -0400

    Added ascii art

--- hydra/save.h
+++ hydra/save.h
@@ -6,6 +6,15 @@
  *
  * @copyright Copyright (c) 2023
  *
+ * 888    888   Y88b   d88P   8888888b.     8888888b.          d8888
+ * 888    888    Y88b d88P    888  "Y88b    888   Y88b        d88888
+ * 888    888     Y88o88P     888    888    888    888       d88P888
+ * 8888888888      Y888P      888    888    888   d88P      d88P 888
+ * 888    888       888       888    888    8888888P"      d88P  888
+ * 888    888       888       888    888    888 T88b      d88P   888
+ * 888    888       888       888  .d88P    888  T88b    d8888888888
+ * 888    888       888       8888888P"     888   T88b  d88P     888
+ *
  * BSD 3-Clause License
  * Copyright (c) 2023, Alvajoy 'Alvajoy123' Asante.
  * All rights reserved.
